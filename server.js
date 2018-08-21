c61ea00fa5adc0fdc2f993e29420b92d1b73d4c3
JD <jt7903@gmail.com>
2018-08-20 21:06:29 -0400

    added extra package

--- server.js
+++ server.js
@@ -4,37 +4,58 @@ var path = require('path');
 var exphbs = require('express-handlebars');
 var aws = require('aws-sdk');
 
-var s3 = new aws.S3({
-  accessKeyId: process.env.S3_KEY,
-  secretAccessKey: process.env.S3_SECRET
-});
+// var s3 = new aws.S3({
+//   db_url: process.env.DATABASE_URL
+// });
 
 var port = process.env.PORT || 5000;
 
-var Sequelize = require('sequelize');
-var sequelize = new Sequelize('cw_session_01', 'root', '', {
-  host: s3.DATABASE_URL,
-  dialect: 'mysql',
-  operatorsAliases: false
-});
+var sequelize = require('sequelize-heroku').connect(require('sequelize'));
 
-const User = sequelize.define('user', {
-  firstName: {
-    type: Sequelize.STRING
-  },
-  lastName: {
-    type: Sequelize.STRING
-  }
-});
-
-// force: true will drop the table if it already exists
-User.sync().then(() => {
-  // Table created
-  return User.create({
-    firstName: 'John',
-    lastName: 'Hancock'
+if (sequelize) {
+  const User = sequelize.define('user', {
+    firstName: {
+      type: Sequelize.STRING
+    },
+    lastName: {
+      type: Sequelize.STRING
+    }
   });
-});
+
+  // force: true will drop the table if it already exists
+  User.sync().then(() => {
+    // Table created
+    return User.create({
+      firstName: 'John',
+      lastName: 'Hancock'
+    });
+  });
+
+
+  sequelize.authenticate().then(function () {
+    var config = sequelize.connectionManager.config;
+    console.log('sequelize-heroku: Connected to ' + config.host + ' as ' + config.username + '.');
+
+    sequelize.query('SELECT 1+1 as test').then(function (res) {
+      console.log('1+1=' + res[0][0].test);
+    });
+
+  }).catch(function (err) {
+    var config = sequelize.connectionManager.config;
+    console.log('Sequelize: Error connecting ' + config.host + ' as ' + config.user + ': ' + err);
+  });
+} else {
+  console.log('No environnement variable found.');
+}
+
+// var Sequelize = require('sequelize');
+// var sequelize = new Sequelize('cw_session_01', 'bd281b14c640dd', '1c629822', {
+//   host: 'mysql://bd281b14c640dd:1c629822@us-cdbr-iron-east-01.cleardb.net/heroku_df77992b0c37b87?reconnect=true',
+//   dialect: 'mysql',
+//   operatorsAliases: false
+// });
+
+
 
  
 
