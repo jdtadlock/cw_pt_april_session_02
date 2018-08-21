cefb334bc64a46a8a9c0f98f168f05d9b17aa9e3
JD <jt7903@gmail.com>
2018-08-21 10:54:51 -0400

    added server code

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var exphbs = require('express-handlebars');
-var aws = require('aws-sdk');
 var Sequelize = require('sequelize');
 var sequelize = require('sequelize-heroku').connect(Sequelize);
 
@@ -12,6 +11,11 @@ var app = express(); // {}
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
 var User = sequelize.define('user', {
